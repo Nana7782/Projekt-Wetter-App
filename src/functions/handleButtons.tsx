74b574d6dc6dbc62e297a33479245b60cadb9b1a
Amme <anne-sophie.nehls@t-online.de>
2024-10-14 14:09:05 +0200

    start

--- src/functions/handleButtons.tsx
+++ src/functions/handleButtons.tsx
@@ -4,15 +4,19 @@ export function handleCity1() {
   const fetchDuesseldorf = fetchWeather(51.2165, 6.7836);
   console.log(fetchDuesseldorf);
 
-  return;
+  return fetchDuesseldorf;
 }
 
 export function handleCity2() {
   const fetchHamburg = fetchWeather(53.550341, 10.000654);
   console.log(fetchHamburg);
+
+  return fetchHamburg;
 }
 
 export function handleCity3() {
   const fetchNuernberg = fetchWeather(49.453872, 11.077298);
   console.log(fetchNuernberg);
+
+  return fetchNuernberg;
 }

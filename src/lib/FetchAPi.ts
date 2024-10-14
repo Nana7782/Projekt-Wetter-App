e601fc44d287237032e5c5f0e9062e122553dbfb
Nana Leven <nana.leven@mail.de>
2024-10-14 11:36:32 +0200

    fetch fn

---
+++ src/lib/FetchAPi.ts
@@ -0,0 +1,13 @@
+import { Weather } from "../modules/Weather";
+import { BASE_URL } from "./API_KEY";
+
+const API_KEY = import.meta.env.API_KEY;
+
+export const fetchWeather = async (
+  lat: number,
+  lon: number
+): Promise<Weather> => {
+  const resp = await fetch(`${BASE_URL}?lat=${lat}&lon${lon}&appid=${API_KEY}`);
+  const json = resp.json();
+  return json;
+};
